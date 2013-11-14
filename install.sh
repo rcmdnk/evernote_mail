2753d156ccde4ffc79ba8a398de7b5c3589017c0
rcmdnk <rcmdnk@gmail.com>
2013-11-14 01:13:22 +0100

    fixed install

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-prefix=/test
 scripts=(https://raw.github.com/rcmdnk/evernote_mail/master/bin/evernote_mail)
-
-
-if [ $# -ne 0 ];then
-  prefix=`echo $1|sed 's|--prefix=||'|sed "s|^~|$HOME|"|sed "s|^\.|$(pwd)|"`
-  flag=0
-  if ! echo "$1"| grep -q "\-\-prefix=";then
-    flag=1
-  elif [ "$prefix" = "" ];then
-    flag=2
-  fi
-  if [ $flag -ne 0 ];then
-    printf "\e[31;2mWrong arguments:\n\e[m" 1>&2
-    printf "\e[31;2m  $@\n\e[m" 1>&2
-    echo 1>&2
-    echo "Use \"$0 --prefix=/your/destination/\"" 1>&2
-    exit $flag
-  fi
+if [ $prefx="" ];then
+  prefix=/usr/local
 fi
+
+prefix=`echo $1|sed 's|--prefix=||'|sed "s|^~|$HOME|"|sed "s|^\.|$(pwd)|"`
+flag=0
+if ! echo "$1"| grep -q "\-\-prefix=";then
+  flag=1
+elif [ "$prefix" = "" ];then
+  flag=2
+fi
+if [ $flag -ne 0 ];then
+  printf "\e[31;2mWrong arguments:\n\e[m" 1>&2
+  printf "\e[31;2m  $@\n\e[m" 1>&2
+  echo 1>&2
+  echo "Use \"$0 --prefix=/your/destination/\"" 1>&2
+  exit $flag
+fi
+
 echo
 echo "######################"
 echo "Install to $prefix/bin"
